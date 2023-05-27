d7d5b999cf605af1937606bfb04e202be164cb0b
Kaymohh <thrilltim@gmail.com>
2023-05-27 23:20:52 +0100

    Added picture handler

--- social-blog-v2/companyblog/models.py
+++ social-blog-v2/companyblog/models.py
@@ -1,9 +1,55 @@
 #models py file
+from companyblog import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import DateTime
 
 
-class User():
-    pass
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
-class Blogpost():
-    pass
+class User(db.Model, UserMixin):
+
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    profile_image = db.Column(db.String(20), nullable=False, default='default_profile.png')
+    email = dc.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    posts = db.relationship('Blogpost', backref='author', lazy=True)
+
+
+    def __init__ (self,email,username,password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        retuen f"Username:  {self.username}"
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    date = db.Column(db.DateTime, nullable = False,default=datetime.utcnow)
+    title = db.Column(db.String(150), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    users = db.relationship(User)
+
+
+    def __init__(self,username,text,title):
+        self.text = text
+        self.title = title
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"Post ID: {self.id} -- Date: {self.date} --- {self.title}"
