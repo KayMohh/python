dfe871d0cb52e46144d15a47cb7770c24c01fd4d
Kaymohh <thrilltim@gmail.com>
2023-05-27 20:57:04 +0100

    views@companyblog

---
+++ social-blog-v2/companyblog/core/views.py
@@ -0,0 +1,20 @@
+## core views
+
+from flask import render_template, request, Blueprint
+
+core = Blueprint("core", __name__)
+
+@core.route('/')
+def index():
+
+
+    return render_template ('index.html')
+
+
+@core.route('/info')
+def info():
+
+    return render_template('info.html')
+@core.route('/login')
+def login():
+    return render_template('login.html')
