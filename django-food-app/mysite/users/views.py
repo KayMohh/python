f47bdad333ba483703d494cdd584bd596ad0c1b0
Kaymohh <thrilltim@gmail.com>
2023-06-02 08:58:20 +0100

    designing user profiles

--- django-food-app/mysite/users/views.py
+++ django-food-app/mysite/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import  UserCreationForm
 from django.contrib import messages
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def register(request):
@@ -11,8 +12,12 @@ def register(request):
                form.save()
                username =form.cleaned_data.get('username')
                messages.success(request, f'Welcome {username}. Your account has been created')
-               return redirect('food:index')
+               return redirect('login')
      else:
           form = RegisterForm()
           
      return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profilepage(request):
+     return render (request, 'users/profile.html')
